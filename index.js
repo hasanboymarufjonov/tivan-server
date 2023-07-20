60533a10b61774ed4faa69db4baa2c6083206ba4
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-07-20 23:24:31 +0500

    .

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ connectDB();
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173",
+    "https://tivan-collector.vercel.app"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"

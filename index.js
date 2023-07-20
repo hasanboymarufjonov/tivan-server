1fb8db8698b3961e265e4fab0def1816c266a602
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-07-20 23:03:37 +0500

    upload server files

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require("express");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./src/routes/userRoutes");
+const { notFound, errorHandler } = require("./src/middlewares/errorMiddleware");
+
+const { connectDB } = require("./src/config/db");
+
+connectDB();
 
 const app = express();
-const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.status(200);
-  res.send("Welcome to root URL of Server");
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, (error) => {
   if (!error)

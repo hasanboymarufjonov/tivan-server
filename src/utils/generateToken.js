5956a7a408e35107842e708ca6917e42960ec2cc
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-07-24 02:26:32 +0500

    4

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -8,7 +8,8 @@ const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    // sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
     domain: ".vercel.app",
   });
